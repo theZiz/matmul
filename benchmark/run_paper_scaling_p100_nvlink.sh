18e11cfa83f2ec0ace49a24672a796e44076612b
Alexander Matthes <a.matthes@mailbox.org>
2017-03-20 17:02:10 +0100

    Added build scripts for planed P3MA submission for Power8 and P100 with NVLINK

---
+++ benchmark/run_paper_scaling_p100_nvlink.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+TILESIZE="4"
+
+rm -rf paper_scaling_p100
+mkdir paper_scaling_p100
+cd paper_scaling_p100
+../make_paper_autotuning_p100.sh $TILESIZE
+./matmul_benchmark 1024 20480 1024 5 > log.txt
+cd ..
