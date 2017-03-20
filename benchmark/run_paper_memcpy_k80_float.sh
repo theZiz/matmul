4b79927ed8d4bf8f13058882cfd77bc72b0a9a26
Alexander Matthes <ziz@mailbox.org>
2017-03-20 17:21:48 +0100

    Adding build and run scripts for K80

---
+++ benchmark/run_paper_memcpy_k80_float.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+TILESIZE="4"
+
+rm -rf paper_memcpy_k80_float
+mkdir paper_memcpy_k80_float
+cd paper_memcpy_k80_float
+../make_paper_memcpy_k80_float.sh $TILESIZE
+./matmul_benchmark 1024 20480 1024 5 > log.txt
+cd ..
