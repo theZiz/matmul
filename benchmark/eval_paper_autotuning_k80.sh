47d7423cdc60cba49d98ce5ce8582bea4ffdd908
Alexander Matthes <ziz@mailbox.org>
2017-03-25 11:19:44 +0100

    Added eval scripts for GPUs

---
+++ benchmark/eval_paper_autotuning_k80.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function doit {
+	cd paper_autotuning_k80_$1
+	cat log$2.txt | grep -v "#" | grep -v "m=n=k" | grep -v "Workdiv" | grep -v "Using" | grep -v -e '^$' | grep -v "Total" | grep "\." | sed "s/	//g"
+	cd ..
+}
+
+function for_matrix {
+	echo "---- matrixsize = $1 ----"
+	doit 1 $1
+	doit 2 $1
+	doit 4 $1
+	doit 8 $1
+}
+
+for_matrix 7168
+for_matrix 10240
+
