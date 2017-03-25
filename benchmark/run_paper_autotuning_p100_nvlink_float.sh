d5795350af6ee0856f1a7d8748af32eb03f200c5
Alexander Matthes <ziz@mailbox.org>
2017-03-25 09:32:01 +0100

    Using smaller matrices for autotuning as slower system need hours for 20480

--- benchmark/run_paper_autotuning_p100_nvlink_float.sh
+++ benchmark/run_paper_autotuning_p100_nvlink_float.sh
@@ -4,8 +4,8 @@ function doit {
 	mkdir paper_autotuning_p100_float_$1
 	cd paper_autotuning_p100_float_$1
 	../make_paper_autotuning_p100_float.sh $1
-	./matmul_benchmark 13312 13312 1 10 > log13312.txt
-	./matmul_benchmark 20480 20480 1 10 > log20480.txt
+	./matmul_benchmark 7168 7168 1 10 > log7168.txt
+	./matmul_benchmark 10240 10240 1 10 > log10240.txt
 	cd ..
 }
 doit 1
